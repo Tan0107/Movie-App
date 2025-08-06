2a7aaf6923216dae7d9a6f051352a1e13ce8f7d2
jthandelosreyesuntalan <jthandelosreyesuntalan@gmail.com>
2025-08-06 13:50:15 +0800

    movie-App

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
 import { AuthProvider } from './contexts/AuthContext'; // path must match where you put it
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </HashRouter>
-  </React.StrictMode>
+    </BrowserRouter>
+  </StrictMode>
 );
 
